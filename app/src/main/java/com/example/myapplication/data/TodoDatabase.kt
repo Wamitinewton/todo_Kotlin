391aa2b277cf189a5901561db10ea8eb999bf5b9
Wamitinewton <wamitinewton@gmail.com>
2024-03-06 21:56:52 +0300

    data layer done

--- app/src/main/java/com/example/myapplication/data/TodoDatabase.kt
+++ app/src/main/java/com/example/myapplication/data/TodoDatabase.kt
@@ -1,5 +1,13 @@
 package com.example.myapplication.data
+import Todo
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database(
+    entities = [Todo:: class],
+    version = 1
+)
 abstract class TodoDatabase: RoomDatabase() {
+
+    abstract  val dao: TodoDao
 }
